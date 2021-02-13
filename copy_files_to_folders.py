80d10d104f5d4ac8154f97dcf84737a2b7889dde
Meatninja <50263765+Meatninja@users.noreply.github.com>
2021-02-13 15:52:31 +0100

    Added exit option
    The program now exits if the provided command is "exit", "quit", or empty.

--- copy_files_to_folders.py
+++ copy_files_to_folders.py
@@ -29,7 +29,7 @@ def ReplaceGroupNamesWithTheirValues(extensionsList: List[str]):  # extensionsLi
 
 
 print('\nThis script will attempt to create new folders for files with the specified extension.\n\n'
-'Type "exit" (no quotation marks) or just hit enter without typing anything to close the script and not do anything.\n\n'
+'Type "exit" or "quit" (no quotation marks) or just hit enter without typing anything to close the script and not do anything.\n\n'
 'Alternatively, if you wish to proceed, simply type in one of the following:\n'
 '1. Names of the extensions you wish to create folders for, separated by spaces. For example:\n'
 'txt\n'
@@ -50,34 +50,37 @@ chosenExtensions = input().split()
 for i in range(0, len(chosenExtensions)):
     chosenExtensions[i] = chosenExtensions[i].replace(',', '').replace('.', '')
 
-RemoveDuplicates(chosenExtensions)
+if ('exit' not in chosenExtensions) & ('quit' not in chosenExtensions) & (len(chosenExtensions) > 0):
+    RemoveDuplicates(chosenExtensions)
 
-ReplaceGroupNamesWithTheirValues(chosenExtensions)
+    ReplaceGroupNamesWithTheirValues(chosenExtensions)
 
-RemoveDuplicates(chosenExtensions)  # in case one of the extensions from the extension groups was explicitly chosen by the user already
+    RemoveDuplicates(chosenExtensions)  # in case one of the extensions from the extension groups was explicitly chosen by the user already
 
-outputDirectoriesNames = chosenExtensions[:]    # cheap hack to copy the list by value, not by reference
+    outputDirectoriesNames = chosenExtensions[:]    # cheap hack to copy the list by value, not by reference
 
-for i in range(0, len(chosenExtensions)):
-    chosenExtensions[i] = '.' + chosenExtensions[i] # txt into .txt, doc into .doc, etc. for ease of use with endswith
-
-
-pathToCurDir = os.path.dirname(os.path.realpath(__file__))
-
-# get all paths for files from the current directory with chosen extensions
-chosenFilePaths = [join(pathToCurDir, curFilename) for curFilename in listdir(pathToCurDir) if (isfile(join(pathToCurDir, curFilename))
-and curFilename.endswith(tuple(chosenExtensions)))]
+    for i in range(0, len(chosenExtensions)):
+        chosenExtensions[i] = '.' + chosenExtensions[i] # txt into .txt, doc into .doc, etc. for ease of use with endswith
 
 
-for filePath in chosenFilePaths:
-    fileExtension = filePath[filePath.rfind('.') + 1 : len(filePath)]   # extension name is after the last dot, all the way to the end of the path
+    pathToCurDir = os.path.dirname(os.path.realpath(__file__))
 
-    outputDirectory = pathToCurDir + '\\' + fileExtension
-
-    if not isdir(outputDirectory):
-        mkdir(outputDirectory)
-
-    copy2(filePath, outputDirectory)    # copy the file (including metadata) to the output directory
+    # get all paths for files from the current directory with chosen extensions
+    chosenFilePaths = [join(pathToCurDir, curFilename) for curFilename in listdir(pathToCurDir) if (isfile(join(pathToCurDir, curFilename))
+    and curFilename.endswith(tuple(chosenExtensions)))]
 
 
-print('Copied files into the following subdirectories: ' + ' '.join(outputDirectoriesNames))
+    for filePath in chosenFilePaths:
+        fileExtension = filePath[filePath.rfind('.') + 1 : len(filePath)]   # extension name is after the last dot, all the way to the end of the path
+
+        outputDirectory = pathToCurDir + '\\' + fileExtension
+
+        if not isdir(outputDirectory):
+            mkdir(outputDirectory)
+
+        copy2(filePath, outputDirectory)    # copy the file (including metadata) to the output directory
+
+
+    print('Copied files into the following subdirectories: ' + ' '.join(outputDirectoriesNames))
+else:
+    print('Program exited. No action taken.')
